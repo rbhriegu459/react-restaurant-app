e310f55c8989ed999750d809fbda19d00e90c885
rbhriegu459 <rbhriegu459@gmail.com>
2024-03-29 23:25:29 +0530

    CloseCartButton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart ={showCartHandler}/>
       <main>
         <Meals />
       </main>
